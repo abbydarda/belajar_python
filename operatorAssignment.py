84347f38bd0926d09db6c54ae7eb126f36aa38f1
abbydarda <darda.abby@gmail.com>
2017-11-27 13:30:59 +0700

    operator assignment

---
+++ operatorAssignment.py
@@ -0,0 +1,30 @@
+# operator = memberikan nilai dikanan kedalam variabel yang ada dikiri
+a = 1
+print a
+
+# operator += Memberikan nilai variabel dengan nilai variabel itu sendiri ditambah dengan nilai di sebelah kanan.
+a += 5
+print a
+
+# operator -= Memberikan nilai variabel dengan nilai variabel itu sendiri dikurang dengan nilai di sebelah kanan.
+a -= 2
+print a
+
+# operator *= Memberikan nilai variabel dengan nilai variabel itu sendiri dikurang dengan nilai di sebelah kanan.
+a *= 2
+print a
+
+# operator /= Memberikan nilai variabel dengan nilai variabel itu sendiri dikurang dengan nilai di sebelah kanan.
+a /= 2
+print a
+
+# operator %= Memberikan nilai variabel dengan nilai variabel itu sendiri dikurang dengan nilai di sebelah kanan.
+a %= 2
+print a
+# operator **= Memberikan nilai variabel dengan nilai variabel itu sendiri dikurang dengan nilai di sebelah kanan.
+a **= 2
+print a
+
+# operator //= Memberikan nilai variabel dengan nilai variabel itu sendiri dikurang dengan nilai di sebelah kanan.
+a //= 2
+print a
