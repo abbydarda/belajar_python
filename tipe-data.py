dfab571c33ef9dbd9ef4f8af4e4bea9bb06c1e2c
abbydarda <darda.abby@gmail.com>
2017-11-27 12:13:19 +0700

    tipe data

---
+++ tipe-data.py
@@ -0,0 +1,31 @@
+# tipe data boolean
+print (True)
+
+# # tipe data string
+print ("Ayo belajar python")
+print ("Belajar pytho sangat mudah")
+#
+# # tipe data integer
+print (123)
+#
+# # tipe data float
+print (3.14)
+
+# # tipe data complex
+print(5j)
+
+# # tipe data list
+print ([1,2,3,4,5])
+print (["satu","dua","tiga","empat","lima"])
+
+# # tipe data tuple
+print ((1,2,3,4,5))
+print (("satu","dua","tiga","empat","lima"))
+
+# # tipe data dictionary
+print ({'nama':'Abby','umur':22})
+
+# # tipe data dictionary dimasukan kedalam variable biodata
+biodata = {'nama':'Abby', 'umur':22} #proses inisialisasi variable biodata
+print (biodata) #proses pencetakan variabel biodata yang berisi tipe data Dictionary
+type (biodata) #fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'> yang berarti dict adalah tipe data dictionary
