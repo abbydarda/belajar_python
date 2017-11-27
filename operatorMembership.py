b2ef948c6c2a5ebfc481748330767623ea5dfe1b
abbydarda <darda.abby@gmail.com>
2017-11-27 13:34:29 +0700

    operator membership

---
+++ operatorMembership.py
@@ -0,0 +1,7 @@
+# operator in Memeriksa apakah nilai yang dicari berada pada list atau struktur data python lainnya. Jika nilai tersebut ada maka kondisi akan bernilai True.
+list = [1,2,3,4,5]
+print 5 in list
+
+# operator not in Memeriksa apakah nilai yang dicari tidak ada pada list atau struktur data python lainnya. Jika nilai tersebut tidak ada maka kondisi akan bernilai True.
+list = [1,2,3,4,5]
+print 10 not in list
