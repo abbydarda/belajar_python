ea0ccdaa56487274dca76a73b7e2cfae7af18666
abbydarda <darda.abby@gmail.com>
2017-11-27 12:25:10 +0700

    variable

---
+++ variable.py
@@ -0,0 +1,30 @@
+# proses memasukan data kedalam variabel
+nama = "Abby Darda Damarullah"
+# proses mencetak variabel
+print (nama)
+
+# nilai dan type data dalam variabel dapat dirubah
+umur = 20
+print (umur)
+print type (umur)
+umur = "dua puluh satu"
+print (umur)
+print type (umur)
+
+namadepan = "Abby Darda"
+namabelakang = "Damarullah"
+nama = namadepan+" "+namabelakang
+umur = 22
+hobi = "tidur"
+print ("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#contoh variabel lainya
+inivariabel = "Halo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
